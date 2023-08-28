ffd9c6f27ed1604d4f238f288b5726d06a8ede18
Kali Sina <ali.sina89@yahoo.com>
2023-08-28 19:38:32 +0200

    Initial Commit

---
+++ ARPlayground/ARPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARPlaygroundApp.swift
+//  ARPlayground
+//
+//  Created by Triumph on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ARPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
